7ef5f8199613e77d7b60668f90a1cf23c6a8f5d4
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-07-04 21:12:56 -0400

    1. Created images folder, stored all the images for the travel journal 2. Creaated travelJournalData for the data 3. Created components folder, and created header and travelCard jsx files in it. 4. Import jsx files in App.jsx 5. Log the data on the console.

---
+++ src/components/TravelCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TravelCard(props){
+  console.log(props.data);
+  return (
+    <div className="travel-card">
+      I'm a travel Card.
+    </div>
+  )
+}
---
+++ src/travelJournalData.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    title: "Peggy's Cove",
+    location: "Nova Scotia, Canada",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Peggys+Cove,+NS/@44.4826985,-63.9519894,13.99z/data=!4m6!3m5!1s0x4b5a02e9d854b0e5:0x6b8306124c808566!8m2!3d44.4939204!4d-63.9140477!16zL20vMDNoOTFx?entry=ttu",
+    startDate: "3rd Oct, 2020",
+    endDate: "3rd Oct, 2020",
+    description:
+      "Nova Scotia is home to over 160 historic lighthouses, but no beacon is as photographed as the one in the vibrant fishing village of Peggy’s Cove. Built in 1915, Peggy’s Point Lighthouse still keeps watch over surging ocean waves and working lobster boats. Amble over giant rocks worn smooth by the sea and share in the view.",
+    imageUrl: "../images/peggysCove.avif",
+  },
+  {
+    id: 2,
+    title: "Montreal Botanical Garden",
+    location: "Montreal, Canada",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Montreal+Botanical+Garden/@45.5561856,-73.5573494,17z/data=!3m1!4b1!4m6!3m5!1s0x4cc91c095abe83c7:0x42a1be0259b47573!8m2!3d45.5561856!4d-73.5547691!16zL20vMDJ2X243?entry=ttu",
+    startDate: "13th May, 2020",
+    endDate: "17th May, 2020",
+    description:
+      "This large botanical garden in the city is part of Montreal Space for Life—Canada’s largest natural sciences museum complex—with over 22,000 species of plants from all over the world, exhibition greenhouses, and thematic gardens. You can learn about Chinese, Japanese and First Nations cultures through their flora, and discover seasonal varieties and events at the main exhibition greenhouse.",
+    imageUrl: "../images/montrealBotanicalGarden.avif",
+  },
+  {
+    id: 3,
+    title: "CN Tower",
+    location: "Toronto, Canada",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "13th July, 2022",
+    endDate: "13th July, 2022",
+    description:
+      "The CN Tower, a prominent symbol of Toronto, Canada, is an architectural marvel that stands at a towering height of 553 meters. Its observation deck provides awe-inspiring panoramic views of the city's skyline and the vast expanse of Lake Ontario. A visit to this iconic landmark is a must for any traveler seeking breathtaking vistas and a true sense of the city's grandeur.",
+    imageUrl:
+      "https://www.google.com/maps/place/CN+Tower/@43.6425662,-79.3896371,17z/data=!3m2!4b1!5s0x882b34d819a55ff7:0xad7cf7bcaf4e239b!4m6!3m5!1s0x882b34d68bf33a9b:0x15edd8c4de1c7581!8m2!3d43.6425662!4d-79.3870568!16zL20vMDF0d3M?entry=ttu",
+  },
+];
