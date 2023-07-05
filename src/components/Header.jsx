57fb1925321338450f1651902875eb5c3a743196
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-07-04 21:37:29 -0400

    finished with header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function Header(){
   return (
     <header>
-      <h1>I am Header.</h1>
+      <img src="./src/images/globe-logo.png" alt="White Colored Globe Logo"  className="header--globe-logo"/>
+      <h3 className="header--title">my travel journal.</h3>
     </header>
   )
 }
