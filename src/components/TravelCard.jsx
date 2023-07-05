2c5132cb52a683e3ed946a32b73c0d80416297d7
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-07-04 21:45:55 -0400

    render data on the page

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 export default function TravelCard(props){
   console.log(props.data);
+  const {description, endDate, googleMapsUrl, imageUrl, location, startDate, title} = props.data
   return (
-    <div className="travel-card">
-      I'm a travel Card.
-    </div>
+    <article className="travel-card">
+      <img src={imageUrl} />
+      <div>
+        <img src="" alt="" />
+        <p>{location}</p>
+        <a href={googleMapsUrl} target="_blank">View on Google Maps</a>
+      </div>
+      <h2>{title}</h2>
+      <p>{startDate} - {endDate}</p>
+      <p>{description}</p>
+    </article>
   )
 }
